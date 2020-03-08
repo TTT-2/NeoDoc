f3f671262ff0a79b0077efb1041098558895466a
Alf21 <4lf-mueller@gmx.de>
2020-03-08 21:51:54 +0100

    Fixed css

--- webfiles/src/App.vue
+++ webfiles/src/App.vue
@@ -21,7 +21,7 @@
 
                 <!-- Search -->
                 <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-green-600 px-2">
-                    <span class="relative w-full">
+                    <div class="relative w-full">
                         <input type="search" placeholder="Search" class="w-full bg-gray-100 text-sm text-green-600 transition border border-transparent focus:outline-none focus:border-green-400 rounded py-1 px-2 pl-10 appearance-none leading-normal">
 
                         <div class="absolute search-icon" style="top: .5rem; left: .8rem;">
@@ -29,7 +29,7 @@
                                 <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
                             </svg>
                         </div>
-                    </span>
+                    </div>
                 </div>
 
                 <!-- Right top side -->
@@ -93,15 +93,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 #main-container {
     background-color: #ffffff;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%23eeeeee' fill-opacity='0.42' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
