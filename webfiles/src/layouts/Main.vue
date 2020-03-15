80393fc2ae4b2df98c69a1c7cea87ccb706d1beb
Alf21 <4lf-mueller@gmx.de>
2020-03-15 21:40:11 +0100

    Added navbar

--- webfiles/src/layouts/Main.vue
+++ webfiles/src/layouts/Main.vue
@@ -1,23 +1,27 @@
 ﻿<template>
     <div class="container">
-        <ul>
-            <li>
-                <v-link href="/home">Home</v-link>
-                <v-link href="/docu/hooks">Hooks</v-link>
-                <v-link href="/docu/createconvars">ConVars</v-link>
-                <v-link href="/docu/datastructures">Functions</v-link>
-            </li>
-        </ul>
+        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div class="flex items-center flex-shrink-0 text-white mr-6">
+                <span class="font-semibold text-xl tracking-tight">NeoDoc</span>
+            </div>
+            <div class="block lg:hidden">
+                <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                </button>
+            </div>
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div class="text-sm lg:flex-grow">
+                    <v-link href="/home" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Home</v-link>
+                    <v-link href="/docu/hooks" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Hooks</v-link>
+                    <v-link href="/docu/createconvars" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">ConVars</v-link>
+                    <v-link href="/docu/datastructures" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Functions</v-link>
+                </div>
+                <div>
+                    <a href="https://github.com/TTT-2/TTT2" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">GitHub</a>
+                </div>
+            </div>
+        </nav>
 
         <slot></slot>
     </div>
 </template>
-
-<style scoped>
-    .container {
-        max-width: 600px;
-        margin: 0 auto;
-        padding: 15px 30px;
-        background: #f9f7f5;
-    }
-</style>
